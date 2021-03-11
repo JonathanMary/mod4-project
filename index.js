ba01879b0c317c6bf9ee21de1bd74546acd1a801
JonathanMary <jon85ma@gmail.com>
2021-03-11 23:01:57 +0100

    some more

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = express();
 
 server.use(express.json());
 server.get('/', (req, res) => {
-    res.send("<h1>HELLO WORLD</h1>");
+    res.send("<h1>HELLO WORLD, from backend</h1>");
 })
 
 const PORT = process.env.PORT || 4000;
