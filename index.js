8f9a241553b42a9a9bd09d2097a04712dd076a18
JonathanMary <jon85ma@gmail.com>
2021-03-11 23:00:39 +0100

    github send

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("WORKS!!")
+require('dotenv').config();
+const express = require('express');
+const server = express();
+
+
+server.use(express.json());
+server.get('/', (req, res) => {
+    res.send("<h1>HELLO WORLD</h1>");
+})
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+})
